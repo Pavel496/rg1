a5169a2e739b459b1720128ef3dc6184bc4126ab
Pavel496 <butusovpn@mail.ru>
2018-05-31 18:14:37 +0300

    Есть

--- resources/views/posts/header.blade.php
+++ resources/views/posts/header.blade.php
@@ -1,9 +1,10 @@
 <header class="container-flex space-between">
   <div class="date">
-    <span class="c-gray-1">{{ $post->published_at->format('M d') }}
-                          / {{ $post->owner->name }}
+    <span class="c-gray-1">{{ optional($post->updated_at)->format('d M Y') }}
+                            {{-- {{ $post->published_at->format('M d') }} --}}
+                          {{-- / {{ $post->owner->name }}
                           / {{ $post->days }}
-                          / {{ $post->hide_at }}
+                          / {{ $post->hide_at }} --}}
     </span>
   </div>
             {{--format('d M Y')--}}
